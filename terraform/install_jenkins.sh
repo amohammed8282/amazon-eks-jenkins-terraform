87a154d38294dee7f08e37e3daeeeead30fa868e
EC2 Default User <ec2-user@ip-10-0-1-150.us-east-2.compute.internal>
2021-10-05 15:10:51 +0000

    daemonize

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -1,11 +1,10 @@
-
-
 #!/bin/bash
 sudo yum -y update
 
 echo "Install Java JDK 8"
 yum remove -y java
-yum install -y java-1.8.0-openjdk
+#yum install -y java-1.8.0-openjdk
+yum install java-11-openjdk-devel
 
 echo "Install Maven"
 yum install -y maven 
@@ -21,9 +20,15 @@ yum install docker -y
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install -y jenkins
+#sudo yum install epel-release # repository that provides 'daemonize'
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+#rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo amazon-linux-extras install epel # repository that provides 'daemonize'
+sudo yum install daemonize -y 
+sudo yum install -y jenkins
+sudo useradd jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
